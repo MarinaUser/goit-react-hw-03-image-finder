bf95c745130643bf88f8d02f2762ebf8a503961e
Marina Bibichenko <108667532+MarinaUser@users.noreply.github.com>
2023-02-08 20:52:19 +0200

    add component searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Searchbar from './Searchbar';
+import { Container } from './App.styled';
+
+
+export class App extends Component {
+  state = {
+    page: 1,
+    query: '',
+    items: [],
+  };
+
+ 
+  handleSearchbarSubmit = query => {
+    // console.log(query);
+    // console.log(this.state);
+    if (this.state.query.toLowerCase() !== query.toLowerCase()) {
+      this.setState({ query, page: 1, items: [] });
+    } else {
+      this.setState({ page: 1 });
+    }
+  };
+
+  
+
+  render() {
+    const { handleSearchbarSubmit } = this;
+
+    return (
+      <Container>
+        <Searchbar onSubmit={handleSearchbarSubmit} />
+      </Container>
+    );
+  }
+}
